d7ccbc02b70b7de6d0ca6a85e0e2063628b56750
lorenzo <lor.abbadessa@gmail.com>
2023-11-08 16:44:54 +0100

    pusha per consiglio

--- js.js
+++ js.js
@@ -10,14 +10,12 @@ for (let i = 1; i <= numCell; i++) {
     
     const cella = document.createElement ("div");  //creo cella (div in html)
     cella.classList.add("cubo");                   //aggiungo la classe alla cella (div)
-    container.appendChild(cella);                  //inseriscco cella (div) in container
-    
+    container.appendChild(cella);                  //inseriscco cella (div) in container   
 
-    let h3 = numCell(i)
-    document.createElement ("h3");         //creo h3 (h3 in html)
-    h3.classList.add("numero");                      //aggiungo la classe all' h3 
-    cella.appendChild(h3);                          //inserisco h3 in cella
-     
+    cella.addEventListener("click", function(){
+        this.classList.add("coloreclick");
+        
+    })
 }
 
 //creare una funzione che vada ad aggiungere una classe che copra il cubo con un altro 
